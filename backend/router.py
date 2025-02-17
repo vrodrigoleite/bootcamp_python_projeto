fa7bd71e49fe28a75c19c33af8e2330100c5fb0d
Victor Almeida <vrodrigoleite@gmail.com>
2025-02-17 17:07:25 -0300

    Finalizando as rotas da API

---
+++ backend/router.py
@@ -0,0 +1,51 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from database import SessionLocal, get_db
+from schema import ProductResponse, ProductUpdate, ProductCreate
+from typing import List
+from crud import (
+    create_product,
+    get_products,
+    get_product,
+    delete_product,
+    update_product,
+)
+
+router = APIRouter()
+
+# Rota para retornar todos os produtos
+@router.get("/products/", response_model=List[ProductResponse])
+def read_all_products_route(db: Session = Depends(get_db)):
+    products = get_products(db)
+    return products
+
+# Rota para retornar um produto específico
+@router.get("/products/{product_id}", response_model=ProductResponse)
+def read_product_route(product_id: int, db: Session = Depends(get_db)):
+    db_product = get_product(db, product_id=product_id)
+    if db_product is None:
+        raise HTTPException(status_code=404, detail="Produto não encontrado!")
+    return db_product
+
+# Rota para criar um novo produto
+@router.post("/products/", response_model=ProductResponse)
+def create_product_route(product: ProductCreate, db: Session = Depends(get_db)):
+    return create_product(db=db, product=product)
+
+# Rota para deletar um produto
+@router.delete("/products/{product_id}", response_model=ProductResponse)
+def detele_product_route(product_id: int, db: Session = Depends(get_db)):
+    db_product = delete_product(db, product_id=product_id)
+    if db_product is None:
+        raise HTTPException(status_code=404, detail="Produto não existe!")
+    return db_product
+
+# Rota para atualizar um pproduto
+@router.put("/products/{product_id}", response_model=ProductResponse)
+def update_product_route(
+    product_id: int, product: ProductUpdate, db: Session = Depends(get_db)
+):
+    db_product = update_product(db, product_id=product_id, product=product)
+    if db_product is None:
+        raise HTTPException(status_code=404, detail="Product not found")
+    return db_product
